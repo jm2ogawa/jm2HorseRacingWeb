e9e63d0e4692d5562e2153ee0192649e65b113c5
jm2ogawa <hirofumiogawa@jm2material.page>
2021-09-29 18:00:33 +0900

    20210929リポジトリ作成
    初回コミット

---
+++ src/main/java/com/example/demo/Jm2HorseRacingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jm2HorseRacingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jm2HorseRacingWebApplication.class, args);
+	}
+
+}
